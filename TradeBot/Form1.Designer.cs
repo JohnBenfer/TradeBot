a7c0fb9e71403970272c921f62a4f1bca767274a
Jbenfer <Jbenfer@DESKTOP-8KA2QAH>
2020-01-24 10:46:24 -0600

    Add project files.

---
+++ TradeBot/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace TradeBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ShareName = new System.Windows.Forms.Label();
+            this.GetPriceButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ShareName
+            // 
+            this.ShareName.AutoSize = true;
+            this.ShareName.Location = new System.Drawing.Point(13, 13);
+            this.ShareName.Name = "ShareName";
+            this.ShareName.Size = new System.Drawing.Size(90, 17);
+            this.ShareName.TabIndex = 0;
+            this.ShareName.Text = "Stock Ticker:";
+            this.ShareName.Click += new System.EventHandler(this.ShareName_Click);
+            // 
+            // GetPriceButton
+            // 
+            this.GetPriceButton.Location = new System.Drawing.Point(16, 47);
+            this.GetPriceButton.Name = "GetPriceButton";
+            this.GetPriceButton.Size = new System.Drawing.Size(98, 46);
+            this.GetPriceButton.TabIndex = 1;
+            this.GetPriceButton.Text = "Get Price";
+            this.GetPriceButton.UseVisualStyleBackColor = true;
+            this.GetPriceButton.Click += new System.EventHandler(this.GetPriceButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.GetPriceButton);
+            this.Controls.Add(this.ShareName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ShareName;
+        private System.Windows.Forms.Button GetPriceButton;
+    }
+}
+
---
+++ TradeBot/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestSharp;
+
+namespace TradeBot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+
+        }
+        RestRequest request;
+        RestClient client;
+        static string baseurl = "wss://ws.finnhub.io?token=";
+        static string key = "bol573vrh5rd0m2j1ktg";
+        static string url = baseurl + key;
+
+
+        private void ShareName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GetPriceButton_Click(object sender, EventArgs e)
+        {
+            client = new RestClient(baseurl);
+            request = new RestRequest("");
+            var content = client.Execute(request).Content;
+        }
+    }
+}

a395264af662378f115a41a5ef5bfed382898278
John Benfer <jbenfer@ksu.edu>
2020-01-25 16:04:29 -0600

    save

--- TradeBot/Form1.cs
+++ TradeBot/Form1.cs
@@ -22,10 +22,20 @@ namespace TradeBot
         }
         RestRequest request;
         RestClient client;
-        static string baseurl = "wss://ws.finnhub.io?token=";
+        static string baseurl = "https://finnhub.io/api/v1/";
+        static string action = "quote?symbol={ticker}&token={key}";
         static string key = "bol573vrh5rd0m2j1ktg";
-        static string url = baseurl + key;
+         
 
+        /*
+         * 
+         * request = new RestRequest("{events}/with/key/{key}", Method.POST);
+            request.AddUrlSegment("key", key);
+            request.AddUrlSegment("events", trigger);
+            var content = client.Execute(request).Content;
+            return true;
+         * 
+         */
 
         private void ShareName_Click(object sender, EventArgs e)
         {
@@ -35,8 +45,14 @@ namespace TradeBot
         private void GetPriceButton_Click(object sender, EventArgs e)
         {
             client = new RestClient(baseurl);
-            request = new RestRequest("");
-            var content = client.Execute(request).Content;
+            request = new RestRequest(action, Method.GET);
+            request.AddUrlSegment("ticker", "AAPL");
+            request.AddUrlSegment("key", key);
+
+            RestRequest r = new RestRequest("quote?symbol=AAPL&token=bol573vrh5rd0m2j1ktg", Method.GET);
+
+            var content = client.Execute(r).Content;
+            Console.WriteLine(content.ToString());
         }
     }
 }

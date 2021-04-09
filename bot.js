6b8607197c17e4e30e2c83d8ccdf7e16d3d59f7e
John Benfer <Johnfbenfer@gmail.com>
2021-04-09 18:14:30 -0500

    initial commit

---
+++ bot.js
@@ -0,0 +1,6 @@
+const axios = require('axios').default;
+const transact = require('./transact');
+
+console.log("Starting Trade Bot");
+
+transact.sell();
---
+++ transact.js
@@ -0,0 +1,9 @@
+const axios = require('axios').default;
+module.exports = {
+  sell: () => {
+    console.log("selling");
+  },
+  buy: () => {
+    console.log("buying");
+  }
+};
